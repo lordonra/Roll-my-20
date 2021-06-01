88669fa8088da9e9493bfe9c0fd9f07013777835
Haru Kaneda <haru_kaneda@icloud.com>
2021-06-01 19:31:16 -0400

    added the dice array . random element to the code. fully functional mine a sigterm error which i think only comes from the simulator and the dice images are small but wil be fixed next

--- Roll my 20/ViewController.swift
+++ Roll my 20/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView: UIImageView!
+    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView.image = #imageLiteral(resourceName: "dice_3")
     }
-
-
+    let diceArray = [#imageLiteral(resourceName: "dice_1"),#imageLiteral(resourceName: "dice_2"),#imageLiteral(resourceName: "dice_3"),#imageLiteral(resourceName: "dice_4"),#imageLiteral(resourceName: "dice_5"),#imageLiteral(resourceName: "dice_6"),#imageLiteral(resourceName: "dice_7"),#imageLiteral(resourceName: "dice_8"),#imageLiteral(resourceName: "dice_9"),#imageLiteral(resourceName: "dice_10"),#imageLiteral(resourceName: "dice_11"),#imageLiteral(resourceName: "dice_12"),#imageLiteral(resourceName: "dice_13"),#imageLiteral(resourceName: "dice_14"),#imageLiteral(resourceName: "dice_15"),#imageLiteral(resourceName: "dice_16"),#imageLiteral(resourceName: "dice_17"),#imageLiteral(resourceName: "dice_18"),#imageLiteral(resourceName: "dice_19"),#imageLiteral(resourceName: "dice_20")]
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView.image = diceArray.randomElement()
+        
+       
+        
+    }
+    
+    
 }
 
